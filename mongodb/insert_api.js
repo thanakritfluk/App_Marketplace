36185c9da42c8a7eb5cf6742a48be1af1bde0ace
thanakritfluk <thanakrit.da@ku.th>
2019-12-10 00:39:37 +0700

    Add async and auto function setup alldata to mongodb"

--- mongodb/insert_api.js
+++ mongodb/insert_api.js
@@ -1,181 +1,149 @@
-var request = require("request");
-const mongo = require('mongodb').MongoClient
-const url = 'mongodb+srv://admin:TI5kgni0wrEkNCX3@cluster0-ngbcy.mongodb.net/test?retryWrites=true&w=majority'
+const axios = require("axios");
+// url for database
+var url = "mongodb+srv://admin:TI5kgni0wrEkNCX3@cluster0-ngbcy.mongodb.net/test?retryWrites=true&w=majority";
+// create a client to mongodb
+var MongoClient = require('mongodb').MongoClient;
+var DataBaseName = "App_marketplace"; 
+// option to api
+const token = "44e5e041bb566e20bf20472dac1624e448796018";
+const limit_number = "100";
+// url to api
+const android_free_url = "https://data.42matters.com/api/v2.0/android/apps/top_google_charts.json?list_name=topselling_free&cat_key=OVERALL&country=US&limit="+limit_number+"&access_token="+token;
+const android_paid_url = "https://data.42matters.com/api/v2.0/android/apps/top_google_charts.json?list_name=topselling_paid&cat_key=OVERALL&country=US&limit="+limit_number+"&access_token="+token;
+const ios_free_url = "https://data.42matters.com/api/v2.0/ios/apps/top_appstore_charts.json?list_name=topselling_free&cat_key=OVERALL&country=US&limit="+limit_number+"&access_token="+token;
+const ios_paid_url = "https://data.42matters.com/api/v2.0/ios/apps/top_appstore_charts.json?list_name=topselling_paid&cat_key=OVERALL&country=US&limit="+limit_number+"&access_token="+token;
 
 
-
-var android_options = { method: 'GET',
-  url: 'https://data.42matters.com/api/v2.0/android/apps/top_google_charts.json',
-  qs: 
-   { list_name: 'topselling_paid',
-     cat_key: 'OVERALL',
-     country: 'US',
-     limit: '100',
-     access_token: '87fcddb37dd051bd89f7979d8e0394b61e37a7f5' },
-  headers: 
-   { 'cache-control': 'no-cache',
-     Connection: 'keep-alive',
-    //  'Accept-Encoding': 'gzip, deflate',
-     Host: 'data.42matters.com',
-     'Postman-Token': '94075fcb-e3e0-4446-8bb9-4546231b5d4c,653a7997-b1c3-4980-8a7b-315ca614abd8',
-     'Cache-Control': 'no-cache',
-     Accept: '*/*',
-     'User-Agent': 'PostmanRuntime/7.20.1' } };
-
-
-request(android_options, function (error, response, body) {
-  if (error) throw new Error(error);
-
-  for(var i = 0; i < 100; i++){
-    let app_name = JSON.parse(body)["app_list"][i]["title"];
-    let app_allrating = JSON.parse(body)["app_list"][i]["rating"];
-    let app_genres = JSON.parse(body)["category_name"];
-    let app_version = JSON.parse(body)["app_list"][i]["version"];
-    let app_screenshot_url = JSON.parse(body)["app_list"][i]["screenshots"];
-    let app_description = JSON.parse(body)["app_list"][i]["description"];
-    let app_restriction = JSON.parse(body)["app_list"][i]["content_rating"];
-    let app_download_amount = JSON.parse(body)["app_list"][i]["downloads_max"];
-    let app_store_url = JSON.parse(body)["app_list"][i]["market_url"];
-    let app_rating1 = JSON.parse(body)["app_list"][i]["ratings_1"];
-    let app_rating2 = JSON.parse(body)["app_list"][i]["ratings_2"];
-    let app_rating3 = JSON.parse(body)["app_list"][i]["ratings_3"];
-    let app_rating4 = JSON.parse(body)["app_list"][i]["ratings_4"];
-    let app_rating5 = JSON.parse(body)["app_list"][i]["ratings_5"];
-    let app_icon = JSON.parse(body)["app_list"][i]["icon"];
-    let app_price = JSON.parse(body)["app_list"][i]["price_numeric"];
-    let app_video = JSON.parse(body)["app_list"][i]["promo_video"];
-    let app_what_new = JSON.parse(body)["app_list"][i]["what_is_new"];
-
-    // console.log(app_name)
-
-    mongo.connect(url, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true
-    }, (err, client) => {
-    if (err) {
-      console.error(err)
-      return
+const getData = async url => {
+    try {
+        const response = await axios.get(url);
+        return response.data;
+    } catch (error) {
+        console.log(error);
     }
-      // console.log("Database Connect")
-      const db = client.db('App_marketplace')
-      const collection = db.collection('Android_app_paid')
-      
-    
-    
-collection.insertOne({
-  App_name: app_name,
-  App_restricition: app_restriction,
-  App_platform: "Android",
-  App_genre: app_genres,
-  App_version:app_version,
-  App_screenshot_url: app_screenshot_url,
-  App_description: app_description,
-  App_allrating: app_allrating,
-  App_download_amount: app_download_amount,
-  App_store_url: app_store_url,
-  App_ratings:[
-    app_rating1,
-    app_rating2,
-    app_rating3,
-    app_rating4,
-    app_rating5
-  ],
-  App_icon: app_icon,
-  App_price: app_price,
-  App_video_url: app_video,
-  App_what_new: app_what_new
-  }, (err, result) => {
-  
-    if (err == null){
-     console.log("Insert complete "+app_name)
+};
+
+
+async function deletes_all(collection_name){
+    const client = await MongoClient.connect(url, { useNewUrlParser: true,useUnifiedTopology: true })
+        .catch(err => { console.log(err); });
+
+    if (!client) {
+        return;
     }
 
-  });
-  client.close();
-});
-  }
-  
+    try {
 
-});
+        const db = client.db(DataBaseName);
 
+        let collection = db.collection(collection_name);
+        let res = await collection.deleteMany({});
 
+    } catch (err) {
+        console.log(err);
+    } finally {
+        console.log("Removed "+collection_name)
+        client.close();
+        
+    }
 
-
-
-var Ios_options = { method: 'GET',
-  url: 'https://data.42matters.com/api/v2.0/ios/apps/top_appstore_charts.json',
-  qs: 
-   { list_name: 'topselling_free',
-     primaryGenreId: '36',
-     device_type: 'iphone',
-     country: 'US',
-     limit: '100',
-     access_token: '87fcddb37dd051bd89f7979d8e0394b61e37a7f5' },
-  headers: 
-   { 'cache-control': 'no-cache',
-     Connection: 'keep-alive',
-     Cookie: '42matters201702=3807a5022e0a6301f24bc16b8e9a3e02',
-    //  'Accept-Encoding': 'gzip, deflate',
-     Host: 'data.42matters.com',
-     'Postman-Token': 'fa4ecbaf-2909-40af-b829-3112811ae982,9bf2d248-3e0a-4b7c-8d4e-b12ab636bc3c',
-     'Cache-Control': 'no-cache',
-     Accept: '*/*',
-     'User-Agent': 'PostmanRuntime/7.17.1' } };
-
-request(Ios_options, function (error, response, body) {
-  if (error) throw new Error(error);
-
-  for(var i = 0; i<100; i++){
-
-  let app_name = JSON.parse(body)["app_list"][i]["trackCensoredName"];
-  let app_allrating = JSON.parse(body)["app_list"][i]["averageUserRating"];
-  let app_genres = JSON.parse(body)["primaryGenreName"];
-  let app_version = JSON.parse(body)["app_list"][i]["version"];
-  let app_screenshot_url = JSON.parse(body)["app_list"][i]["screenshotUrls"];
-  let app_description = JSON.parse(body)["app_list"][i]["description"];
-  let app_restriction = JSON.parse(body)["app_list"][i]["trackContentRating"];
-  let app_download_amount = JSON.parse(body)["app_list"][i]["userRatingCount"];
-  let app_store_url = JSON.parse(body)["app_list"][i]["trackViewUrl"];
-  let app_icon = JSON.parse(body)["app_list"][i]["artworkUrl100"];
-  let app_price = JSON.parse(body)["app_list"][i]["price"];
-  let app_what_new = JSON.parse(body)["app_list"][i]["releaseNotes"];
-
-
-  mongo.connect(url, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-  }, (err, client) => {
-  if (err) {
-    console.error(err)
-    return
-  }
-  // console.log("Database Connect")
-  const db = client.db('App_marketplace')
-  const collection = db.collection('Ios_app_free')
-  
-
-collection.insertOne({
-  App_name: app_name,
-  App_restricition: app_restriction,
-  App_platform: "Ios",
-  App_genre: app_genres,
-  App_version:app_version,
-  App_screenshot_url: app_screenshot_url,
-  App_description: app_description,
-  App_allrating: app_allrating,
-  App_download_amount: app_download_amount,
-  App_store_url: app_store_url,
-  App_icon: app_icon,
-  App_price: app_price,
-  App_what_new: app_what_new
-  }, (err, result) => {
-    if (err == null){
-      console.log("Insert complete "+ app_name)
-     }
-});  
-client.close();
-
-});
 }
 
-});
+
+async function insertOne(os_name,data,collection_name,time) {
+
+    const client = await MongoClient.connect(url, { useNewUrlParser: true,useUnifiedTopology: true })
+        .catch(err => { console.log(err); });
+
+    if (!client) {
+        return;
+    }
+
+    try {
+
+        const db = client.db(DataBaseName);
+
+        let collection = db.collection(collection_name);
+        let query;
+        if(os_name=="Android"){
+            query = { 
+                App_name: data["title"],
+                App_restriction: data["content_rating"],
+                App_platform: "Android",
+                App_genre: data["category"],
+                App_version:data["version"],
+                App_screenshot_url: data["screenshots"],
+                App_description: data["description"],
+                App_allrating: data["rating"],
+                App_download_amount: data["downloads_max"],
+                App_store_url: data["market_url"],
+                App_ratings:[
+                    data["ratings_1"],
+                    data["ratings_2"],
+                    data["ratings_3"],
+                    data["ratings_4"],
+                    data["ratings_5"]
+                ],
+                App_icon: data["icon"],
+                App_price: data["price_numeric"],
+                App_video_url: data["promo_video"],
+                App_what_new: data["what_is_new"]
+            
+            }
+
+        }
+        if(os_name=="Ios"){
+
+            query = {
+                App_name: data["trackCensoredName"],
+                App_restriction: data["trackContentRating"],
+                App_platform: "Ios",
+                App_genre: data["primaryGenreName"],
+                App_version:data["version"],
+                App_screenshot_url: data["screenshotUrls"],
+                App_description: data["description"],
+                App_allrating: data["averageUserRating"],
+                App_download_amount: data["userRatingCount"],
+                App_store_url: data["trackViewUrl"],
+                App_icon: data["artworkUrl100"],
+                App_price: data["price"],
+                App_what_new: data["releaseNotes"]
+            }
+        }
+
+        let res = await collection.insertOne(query);
+
+        if(os_name=="Android"){
+        console.log("Inserted "+"No."+time+" "+collection_name+": " + data["title"]);
+        }
+        if(os_name=="Ios"){
+            console.log("Inserted "+"No."+time+" "+collection_name+": " + data["trackCensoredName"]);
+        }
+
+    } catch (err) {
+        console.log(err);
+    } finally {
+        client.close();
+    }
+}
+
+
+async function insert_data(os_name,url,collection_name){
+    var data = await getData(url);
+
+    for(var i = 0; i<100 ; i++){
+        var insert = await insertOne(os_name,data["app_list"][i],collection_name,(i+1))
+    }
+
+}
+
+async function setup_all_data(){
+    var delete_android_paid = await deletes_all('Android_app_paid');
+    var delete_android_paid = await deletes_all('Android_app_free');
+    var delete_android_paid = await deletes_all('Ios_app_paid');
+    var delete_android_paid = await deletes_all('Ios_app_free');
+    var android_paid_app = await insert_data("Android",android_paid_url,'Android_app_paid');
+    var android_free_app = await insert_data("Android",android_free_url,'Android_app_free');
+    var ios_paid_app = await insert_data("Ios",ios_paid_url,"Ios_app_paid")
+    var ios_free_app = await insert_data("Ios",ios_free_url,"Ios_app_free")
+}
