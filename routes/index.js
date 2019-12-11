2d148c86eedf743b505b32add0299c732c91a5d3
Katapon Sinpunyawong <q.katapol@gmail.com>
2019-12-11 23:24:43 +0700

    Add: get list data from App_marketplace

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,40 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb+srv://admin:"+process.env.MONGO_PASS+"@cluster0-ngbcy.mongodb.net/test?retryWrites=true&w=majority";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
+
+router.get('/List', function(req, res, next) {
+   MongoClient.connect(url, function(err, db){
+     if(err){
+       console.log('Cannot connect to the server');
+     } else {
+      console.log('Connection Established');
+
+      var collection = db.collection('App_marketplace');
+
+      collection.find({}).toArray(function(err, result) {
+        if (err){
+          res.send(err);
+        }else if (result.length) {
+          res.render('App_marketplace_list', {
+            "App_marketplace_list" : result
+          });
+        } else {
+          res.send('No documents found');
+        }
+
+        db.close();
+      });
+     }
+   });
+});
+
+
 
 module.exports = router;
