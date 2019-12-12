aba0cfb22b692637c6cefed0584198d00426afa5
Raksani <raksani.k@ku.th>
2019-12-12 20:14:14 +0700

    finish index page

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -104,3 +104,9 @@ $(document).ready(function () {
     }
 
 });
+
+$('.round').click(function(e) {
+    e.preventDefault();
+    e.stopPropagation();
+    $('.arrow').toggleClass('bounceAlpha');
+});
