19da819e0772814d160a6967c1466ebbc850c49c
Poonnanun <poonnanun.p@ku.th>
2019-12-14 02:05:25 +0700

    still bug

--- routes/index.js
+++ routes/index.js
@@ -2,39 +2,42 @@ var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
 
-var url = "mongodb+srv://admin:"+process.env.MONGO_PASS+"@cluster0-ngbcy.mongodb.net/test?retryWrites=true&w=majority";
+var url = "mongodb+srv://reader:mr3TWRw1RcJeAQIt@cluster0-ngbcy.mongodb.net/test?retryWrites=true&w=majority";
+
+var getDashboard = function () {
+  var promise = new Promise(function (resolve, reject) {
+    MongoClient.connect(url, function(err, db){
+      if(err){
+        reject('Cannot connect to the server');
+      } else {
+        console.log('Connection Established');
+        var dbo = db.db("App_marketplace");
+        // dbo.collection("Android_app_free").find({}, { projection: { _id: 0, App_name:1} }).toArray(function(err, result) {
+        //   if (err) reject(err);
+        //   resolve(result[0]);
+        //   db.close();
+        // });
+        dbo.collection("Android_app_free").findOne({}, { projection: { _id: 0, App_name: 1}}, function(err, result) {
+          if (err) reject(err);
+          resolve(result);
+          db.close();
+        });
+        // resolve(dbo.collection("Android_app_free").count())
+      }
+    }); 
+  });
+  return promise;
+};
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  getDashboard()
+  .then(function success(result) {
+    res.render('index',  { 
+         data: result,
+         title: 'Express',           
+     });
+ })
 });
-
-router.get('/List', function(req, res, next) {
-   MongoClient.connect(url, function(err, db){
-     if(err){
-       console.log('Cannot connect to the server');
-     } else {
-      console.log('Connection Established');
-
-      var collection = db.collection('App_marketplace');
-
-      collection.find({}).toArray(function(err, result) {
-        if (err){
-          res.send(err);
-        }else if (result.length) {
-          res.render('App_marketplace_list', {
-            "App_marketplace_list" : result
-          });
-        } else {
-          res.send('No documents found');
-        }
-
-        db.close();
-      });
-     }
-   });
-});
-
-
 
 module.exports = router;
