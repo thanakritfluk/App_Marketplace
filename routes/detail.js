bcf538d23e28a30f301563fcb3f6fa871c7332f2
thanakritfluk <thanakrit.da@ku.th>
2019-12-15 02:21:33 +0700

    Add function to find is app exist in which collection wait for ou to get url below and don't forget to remove in ejs file tag available

--- routes/detail.js
+++ routes/detail.js
@@ -1,15 +1,50 @@
 var express = require('express');
 var router = express.Router();
 var MongoClient = require('mongodb').MongoClient;
-
 var url = "mongodb+srv://reader:mr3TWRw1RcJeAQIt@cluster0-ngbcy.mongodb.net/test?retryWrites=true&w=majority";
+var DataBaseName = "App_marketplace"; 
+var collection_available = [];
+
+
+async function find_is_exist(app_name,collection_name){
+  const client = await MongoClient.connect(url, { useNewUrlParser: true,useUnifiedTopology: true })
+      .catch(err => { console.log(err); });
+  if (!client) {
+      return;
+  }
+  try {
+      const db = client.db(DataBaseName);
+
+      let collection = db.collection(collection_name);
+      let res = await collection.findOne({App_name: app_name});
+      if(res == null){
+          console.log('Not Found In '+collection_name)
+      }else{
+        collection_available.push(collection_name)
+      }
+  } catch (err) {
+      console.log(err);
+  } finally {
+      client.close();
+  }
+}
+
+
 
 /* GET home page. */
 router.get('/:name', async function(req, res, next) {
-  var name = req.params.name;
-  getDetail(name)
+  var app_name = req.params.name;
+ 
+  var ios_free = await find_is_exist(app_name,'Ios_app_free');
+  var android_free = await find_is_exist(app_name,'Android_app_free');
+  var ios_paid = await find_is_exist(app_name,'Ios_app_paid');
+  var android_paid = await find_is_exist(app_name,'Android_app_paid');
+
+  await getDetail(app_name)
   .then(function success(result) {
-     res.render('detail', { detail: result[0]});
+    console.log(collection_available)
+     res.render('detail', { detail: result[0], url:collection_available});
+     collection_available = [];
   })
 });
 
@@ -33,7 +68,7 @@ var getDetail = function (name) {
           if(ans[0] == null){
             console.log(".....")
           }else{
-            console.log(ans)
+            // console.log(ans)
             resolve(ans);
           }
           db.close();
@@ -52,4 +87,5 @@ var getDetail = function (name) {
   });
   return promise;
 };
+
 module.exports = router;
