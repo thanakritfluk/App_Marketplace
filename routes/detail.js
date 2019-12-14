08c8702b4e0aa3dabcd1b22fb05ddf7f383c2ea2
Poonnanun <poonnanun.p@ku.th>
2019-12-14 16:25:22 +0700

    init

--- routes/detail.js
+++ routes/detail.js
@@ -3,7 +3,11 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('detail', { title: 'detail' });
+  res.render('detail', { title: 'detail'});
 });
 
+router.get('/:name', function(req, res, next) {
+  var name = req.params.name;
+  res.render('detail', { title: name});
+});
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Express' ,test: 'Instragram'});
+});
+
+router.get('/cate/:cate', function(req, res, next) {
+  var cate = req.params.cate;
+  res.render('index', { title: 'Express' ,test: 'Instragram'});
+});
+
+router.get('/search/:search', function(req, res, next) {
+  var search = req.params.search;
+  res.render('index', { title: 'Express' ,test: 'Instragram'});
 });
 
 module.exports = router;
