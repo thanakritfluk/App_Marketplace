d59e0dc4b5a158224f35d21fa3ba2eec479faa72
Poonnanun <poonnanun.p@ku.th>
2019-12-14 20:03:00 +0700

    tmp

--- routes/detail.js
+++ routes/detail.js
@@ -1,13 +1,34 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
+
+var url = "mongodb+srv://reader:mr3TWRw1RcJeAQIt@cluster0-ngbcy.mongodb.net/test?retryWrites=true&w=majority";
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('detail', { title: 'detail'});
-});
-
 router.get('/:name', function(req, res, next) {
   var name = req.params.name;
-  res.render('detail', { title: name});
+  getDetail(name)
+  .then(function success(result) {
+     res.render('detail', { detail: result});
+  })
 });
+
+var getDetail = function (name) {
+  var promise = new Promise(function (resolve, reject) {
+    MongoClient.connect(url, function(err, db){
+      if(err){
+        reject('Cannot connect to the server');
+      } else {
+        console.log('Connection Established');
+        var dbo = db.db("App_marketplace");
+        dbo.collection("Android_app_free").findOne({App_name: name}).then((function(err, result) {
+          if (err) reject(err);
+          resolve(result);
+          db.close();
+        }));
+      }
+    }); 
+  });
+  return promise;
+};
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,21 @@ var MongoClient = require('mongodb').MongoClient;
 
 var url = "mongodb+srv://reader:mr3TWRw1RcJeAQIt@cluster0-ngbcy.mongodb.net/test?retryWrites=true&w=majority";
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  getTopPaid()
+router.get('/', async function(req, res, next) {
+  var paid
+  var free
+  await getTopPaid()
     .then(function success(result) {
-       res.render('index',  { 
-            data: result       
-        });
+       paid = result;
     })
+  await getTopFree()
+    .then(function success(result) {
+       free = result;
+    })
+  res.render('index',  { 
+    paid: paid,
+    free: free
+  });
 });
 
 router.get('/cate/:cate', function(req, res, next) {
@@ -31,7 +39,31 @@ var getTopPaid = function () {
       } else {
         console.log('Connection Established');
         var dbo = db.db("App_marketplace");
-        dbo.collection("Android_app_free").find({}).sort({App_allrating:1}).limit(2).toArray(function(err, result) {
+        var all = []
+        dbo.collection("Android_app_paid").find({}).sort({App_allrating:-1}).limit(2).toArray(function(err, result) {
+          if (err) reject(err);
+          var ans = []
+          for(let i=0; i<2; i++){
+            ans.push(result[i])
+          }
+          resolve(ans);
+          db.close();
+        });
+      }
+    }); 
+  });
+  return promise;
+};
+
+var getTopFree = function () {
+  var promise = new Promise(function (resolve, reject) {
+    MongoClient.connect(url, function(err, db){
+      if(err){
+        reject('Cannot connect to the server');
+      } else {
+        console.log('Connection Established');
+        var dbo = db.db("App_marketplace");
+        dbo.collection("Android_app_free").find({}).sort({App_allrating:-1}).limit(2).toArray(function(err, result) {
           if (err) reject(err);
           var ans = []
           for(let i=0; i<2; i++){
