85b47bbcb74bb8c8db5b67577746d205ecf054b0
Poonnanun <poonnanun.p@ku.th>
2019-12-15 02:52:41 +0700

    ancient search engine

--- routes/search.js
+++ routes/search.js
@@ -5,16 +5,16 @@ var MongoClient = require('mongodb').MongoClient;
 var url = "mongodb+srv://reader:mr3TWRw1RcJeAQIt@cluster0-ngbcy.mongodb.net/test?retryWrites=true&w=majority";
 
 /* GET home page. */
-router.get('/:name', async function(req, res, next) {
-  var name = req.params.name;
-//   getDetail(name)
-//   .then(function success(result) {
-//      res.render('detail', { detail: result[0]});
-//   })
-res.render('search', { detail: '..'});
+router.get('/:search', async function(req, res, next) {
+    var name = req.params.search;
+    getApp(name)
+        .then(function success(result) {
+            console.log(result.length)
+            res.render('search', { App: result})
+        });
 });
 
-var getDetail = function (name) {
+var getApp = function (name) {
   var promise = new Promise(function (resolve, reject) {
     var search = name.replace("%20", "_");
     MongoClient.connect(url, function(err, db){
@@ -23,26 +23,24 @@ var getDetail = function (name) {
       } else {
         console.log('Connection Established');
         var dbo = db.db("App_marketplace");
-        dbo.collection("Android_app_paid").find({App_name: search}).limit(1).toArray((function(err, result) {
+        dbo.collection("Android_app_paid").find({App_name: search}).toArray((function(err, result) {
           if (err) {
             console.log(err);
           }
           var ans = []
-          for(let i=0; i<1; i++){
+          for(let i=0; i<result.length; i++){
             ans.push(result[i])
           }
-          if(ans[0] == null){
-            console.log(".....")
+          if(ans[0] == null){          
           }else{
-            console.log(ans)
             resolve(ans);
           }
           db.close();
         }));
-        dbo.collection("Android_app_free").find({App_name: search}).limit(1).toArray((function(err, result) {
+        dbo.collection("Android_app_free").find({App_name: search}).toArray((function(err, result) {
           if (err) reject(err);
           var ans = []
-          for(let i=0; i<1; i++){
+          for(let i=0; i<result.length; i++){
             ans.push(result[i])
           }
           resolve(ans);
