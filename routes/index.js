db21fb647b3657f3315efec137ead7a49ff38523
Poonnanun <poonnanun.p@ku.th>
2019-12-14 17:44:33 +0700

    tmp

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var MongoClient = require('mongodb').MongoClient;
 
+var url = "mongodb+srv://reader:mr3TWRw1RcJeAQIt@cluster0-ngbcy.mongodb.net/test?retryWrites=true&w=majority";
 /* GET home page. */
 router.get('/', function(req, res, next) {
   getTopPaid()
     .then(function success(result) {
        res.render('index',  { 
-            data: result,
-            title: 'DAQ Basketball prediction',
-            current_year: current_year,            
+            data: result       
         });
     })
 });
